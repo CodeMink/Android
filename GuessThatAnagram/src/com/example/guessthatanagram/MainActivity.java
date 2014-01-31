8b89ee2ad847eed8de96f1c13485d316dac67103
CodeMink <jessica.shen97@gmail.com>
2014-01-31 14:40:48 -0500

    I'M SO CONFUSED

---
+++ GuessThatAnagram/src/com/example/guessthatanagram/MainActivity.java
@@ -0,0 +1,64 @@
+package com.example.guessthatanagram;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Set;
+import java.util.TreeSet;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.res.AssetManager;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+
+public class MainActivity extends Activity{
+	
+	public static final Set<String> DICTIONARY = new TreeSet<String> ();
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState){
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		AssetManager assMan = getAssets();
+		InputStream n = null;
+		try {
+			n = assMan.open("Dictionary.txt");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		BufferedReader in = new BufferedReader(new InputStreamReader(n));
+		
+		String s;
+		try {
+			while((s=in.readLine()) != null)
+				DICTIONARY.add(s);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		Intent intent = new Intent(this, Game.class);
+		startActivity(intent);
+		
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+	
+	public void startGame(View view)
+	{
+		Intent intent = new Intent(this, Game.class);
+		startActivity(intent);
+	}
+
+}
---
+++ GuessThatAnagram/src/com/example/guessthatanagram/Trie.java
@@ -0,0 +1,26 @@
+package com.example.guessthatanagram;
+
+import java.util.ArrayList;
+
+public class Trie {
+	
+	public static void main(String[] args)
+	{
+		CharSequence c = new String("dfs");
+		System.out.println("asdf".contains(c));
+	}
+	
+	//sorts strings efficiently
+//	private ArrayList<Character> begin;
+//	public Trie()
+//	{
+//		
+//	}
+//	
+//	
+//	private class leaf
+//	{
+//		public leaf(char val, )
+//	}
+
+}
